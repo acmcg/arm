9070f17c0b368098837f98ffc6bdbc562f566e74
Andrew McGregor <andrew.mcgregor@veritec.com.au>
2019-03-18 15:47:15 +1100

    Extract data from ISM

---
+++ ISM-Read.ps1
@@ -0,0 +1,32 @@
+﻿# Usage ./ISM-Read.ps1 | convertto-csv -notypeinformation | out-file controls.csv
+
+$Word = New-Object -ComObject Word.Application
+$Document = $Word.Documents.Open('C:\Users\Andrew.McGregor\ISM.docx')
+$Word.Visible = $False
+$controlObjects = @()
+foreach ($paragraphs in $Document.Paragraphs) 
+{ 
+    if($paragraphs.Style.NameLocal -eq "Heading 1"){
+        $ismSection = $paragraphs.range.Text
+    }
+    # Select only the text 
+    If ($paragraphs.Range.Font.ColorIndex -eq 15) 
+    { 
+       if ($paragraphs.Range.Text -match "Security Control:"){
+            $paragraphText = $paragraphs.Range.Text -replace "\v",";" #Remove vertical tab 
+            $paragraphArray = $paragraphText -Split "Security Control: |; Revision: |; Updated: |; Applicability: |; Priority: |;" # Split string and remove headings
+            $paragraphArray = $paragraphArray.ForEach("Trim") # remove leading and trailng whitespaces
+            # start array from position 1 as position 0 is empty due to the way split works
+            $controlObjects += [pscustomobject]@{ISMSection = $ismSection;Control = $paragraphArray[1];Revision = $paragraphArray[2];Updated = $paragraphArray[3];Classification = $paragraphArray[4];Priority = $paragraphArray[5];Description = $paragraphArray[6] }
+        }
+        else{
+            #loop on paragraphs as word list items are individual paragraphs
+            $controlObjects[-1].Description = "$($controlObjects[-1].Description) `n -$($paragraphs.Range.Text)"
+        }
+    } 
+
+} 
+$controlObjects 
+$Word.Documents.Close()
+
+
